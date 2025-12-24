becca414bc325461e5cc00fce1c9d25f6fce27f5
agil_mathews <agilmathewss@gmail.com>
2025-12-24 10:39:05 +0530

    first commit

---
+++ lib/core/themes/text_themes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:alkor_shopin/core/constant/app_fonts.dart';
+
+TextTheme appTextTheme = TextTheme(
+  bodyLarge: const TextStyle(fontFamily: AppFonts.dmSans),
+);
